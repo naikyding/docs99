c9e595c9ca3c8a6c71bda58241698f586bd9e1bc
NaikyDing <74ding@gmail.com>
2021-11-09 14:58:43 +0800

    feat: config titile && description

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Hello VitePress',
-  description: 'Just playing around.',
+  title: 'Docs99',
+  description: '開發相關的認知與學習，期望學習長長99',
 
   themeConfig: {
     sidebar: {
