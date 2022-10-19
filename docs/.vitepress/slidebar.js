f5f7b39d4c0e28818a3dc98fc2ff930103a85b78
Naiky <74ding@gmail.com>
2022-10-19 16:32:34 +0800

    feat(css): remove atomic css doc

--- docs/.vitepress/slidebar.js
+++ docs/.vitepress/slidebar.js
@@ -229,10 +229,6 @@ export const getCssSidebar = () => ({
       text: '滑鼠任意拖拽元素停留效果',
       link: '/css/mouse-follow-drag'
     },
-    {
-      text: '原子化 CSS',
-      link: '/css/atomic-css'
-    },
   ]
 })
 
